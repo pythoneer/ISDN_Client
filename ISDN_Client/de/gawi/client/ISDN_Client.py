acf098ec486e566af27a47516e2183b46896e366
Dustin <dustin.bensing@googlemail.com>
2011-12-16 11:13:06 +0100

    notification versuch

--- ISDN_Client/de/gawi/client/ISDN_Client.py
+++ ISDN_Client/de/gawi/client/ISDN_Client.py
@@ -16,20 +16,28 @@ html = """
 """
 
 app = QApplication(sys.argv)
+screen_x = app.desktop().screenGeometry().width()
+screen_y = app.desktop().screenGeometry().height()
+
 view = QWebView()
 
 class PreviewWindow(PyQt4.QtGui.QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, message=""):
         super(PreviewWindow, self).__init__(parent)
 
-        self.label = QtGui.QLabel("Nachricht!")
-
+        self.label = QtGui.QLabel("Nachricht:" + message)
+        self.parent = parent
+        
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.label)
         self.setLayout(layout)
 
         self.setWindowTitle("Preview")
-
+        
+        #self.move(screen_x - (self.geometry().width() + 160), screen_y - 120)
+        self.setGeometry(screen_x - (self.geometry().width() + 170), screen_y - 160 ,250,100)
+        self.setStyleSheet("background-color:#bbb;")
+        
     def mousePressEvent(self, event):
         print "mouse"
         self.close()
@@ -43,7 +51,7 @@ class ConsolePrinter(QObject):
         print message
         flags = QtCore.Qt.ToolTip
         flags |= QtCore.Qt.WindowStaysOnTopHint
-        previewWindow = PreviewWindow(view)
+        previewWindow = PreviewWindow(view, message)
         previewWindow.setWindowFlags(flags)
         previewWindow.show()
         print "now"
@@ -60,9 +68,9 @@ if __name__ == '__main__':
     #printer = ConsolePrinter()
     #view.setHtml(html)
     QtCore.QObject.connect(frame, QtCore.SIGNAL('javaScriptWindowObjectCleared()'), loadJavaScriptObjects)
-    view.load(QUrl('http://localhost:8080/Vaadin_remote_executor/?debug'))
+    view.load(QUrl('http://localhost:8080/ISDN_Vaadin_Server/?debug'))
     #frame.addToJavaScriptWindowObject('printer', printer)
-    frame.evaluateJavaScript("alert('Hello');")
+    #frame.evaluateJavaScript("alert('Hello');")
     #frame.evaluateJavaScript("printer.text('Goooooooooo!');")
     view.show()
     app.exec_()
